7131a627677c86450b35b158de3d383ea0ff18b5
yerikshu <treestore@foxmail.com>
2022-09-17 15:26:22 +0800

    配置总线

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import mitt from 'mitt'
 
 import "./assets/main.css";
 import ElementPlus from 'element-plus'
@@ -15,5 +16,7 @@ const app = createApp(App);
 app.use(createPinia());
 app.use(router);
 app.use(ElementPlus,{ locale: zhCn})
+// vue3.x的全局实例，要挂载在config.globalProperties上
+app.config.globalProperties.$EventBus = new mitt()
 
 app.mount("#app");
