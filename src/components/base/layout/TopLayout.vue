5677128a425905a63e111f0c9a8ce38245657b6d
yerikshu <treestore@foxmail.com>
2022-09-18 16:22:13 +0800

    menu

---
+++ src/components/base/layout/TopLayout.vue
@@ -0,0 +1,19 @@
+<template>
+  <el-container>
+    <el-header style="background-color: #fff; padding: 0px"> </el-header>
+    <el-main>
+      <div style="border-radius: 10px; background-color: #fff; padding: 5px">
+        <router-view v-if="reloadFlag" />
+      </div>
+    </el-main>
+  </el-container>
+</template>
+<script>
+import { ref } from "vue";
+export default {
+  setup() {
+    const reloadFlag = ref(true);
+    return reloadFlag;
+  },
+};
+</script>
