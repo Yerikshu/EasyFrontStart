34d2415627e435ea4cb4a79e990cff03ae542a8d
yerikshu <treestore@foxmail.com>
2022-09-22 23:24:46 +0800

    配置搭载

---
+++ src/stores/index.js
@@ -0,0 +1,25 @@
+import { createStore } from 'vuex'
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+
+// Vite supports importing multiple modules from the file system using the special import.meta.glob function
+// see https://cn.vitejs.dev/guide/features.html#glob-import
+const modulesFiles = import.meta.globEager('./modules/*.js')
+const pathList = []
+
+for (const path in modulesFiles) {
+  pathList.push(path)
+}
+
+const modules = pathList.reduce((modules, modulePath) => {
+  const moduleName = modulePath.replace(/^\.\/modules\/(.*)\.\w+$/, '$1')
+  const value = modulesFiles[modulePath]
+  modules[moduleName] = value.default
+  return modules
+}, {})
+
+const store = createStore({
+  modules
+})
+
+export default store
