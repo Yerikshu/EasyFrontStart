22492b144dfcaddeba2f8b18eff97baae5e5f2ec
Yerikshu <treestore@foxmail.com>
2022-10-12 14:39:20 +0800

    Update Footer.vue

--- src/components/base/layout/Footer.vue
+++ src/components/base/layout/Footer.vue
@@ -1,3 +1,15 @@
 <template>
-    Copyright
+  <div style="text-align: center">
+    <div>
+      <el-button link>帮助</el-button>
+      <el-button link>声明</el-button>
+      <el-button link>介绍</el-button>
+    </div>
+    <div style="margin-top: 10px">
+      copyright
+    </div>
+  </div>
 </template>
+<script setup>
+
+</script>
