909883a7aa7bad3013affa77ea8a626f1b9c1b77
yerikshu <treestore@foxmail.com>
2022-10-16 17:23:55 +0800

    配置404

--- src/components/base/common/MCTree.vue
+++ src/components/base/common/MCTree.vue
@@ -1,48 +1,50 @@
 <template>
   <slot v-if="finalConfig.withHeaderSlot" name="headerSlot" />
-  <el-tree
-    ref="tree"
-    v-loading="loading"
-    :data="finalConfig.data"
-    :empty-text="finalConfig.emptyText"
-    :node-key="finalConfig.nodeKey"
-    :props="finalConfig.props"
-    :render-after-expand="finalConfig.renderAfterExpand"
-    :load="finalConfig.load"
-    :render-content="finalConfig.renderContent"
-    :highlight-current="finalConfig.highlightCurrent"
-    :default-expand-all="finalConfig.defaultExpandAll"
-    :expand-on-click-node="finalConfig.expandOnClickNode"
-    :check-on-click-node="finalConfig.checkOnClickNode"
-    :auto-expand-parent="finalConfig.autoExpandParent"
-    :default-expanded-keys="finalConfig.defaultCheckedKeys"
-    :show-checkbox="finalConfig.showCheckbox"
-    :check-strictly="finalConfig.checkStrictly"
-    :default-checked-keys="finalConfig.defaultCheckedKeys"
-    :current-node-key="finalConfig.currentNodeKey"
-    :filter-node-method="finalConfig.filterNodeMethod"
-    :accordion="finalConfig.accordion"
-    :indent="finalConfig.indent"
-    :icon="finalConfig.icon"
-    :lazy="finalConfig.lazy"
-    :draggable="finalConfig.draggable"
-    :allow-drag="finalConfig.allowDrag"
-    :allow-drop="finalConfig.allowDrop"
-    @node-click="handleNodeClick"
-    @node-contextmenu="handleNodeContextmenu"
-    @check-change="handleCheckChange"
-    @check="handleCheck"
-    @current-change="handleCurrentChange"
-    @node-expand="handleNodeExpand"
-    @node-collapse="handleNodeCollapse"
-    @node-drag-start="handleNodeDragStart"
-    @node-drag-enter="handleNodeDragEnter"
-    @node-drag-leave="handleNodeDragLeave"
-    @node-drag-over="handleNodeDragOver"
-    @node-drag-end="handleNodeDragEnd"
-    @node-drop="handleNodeDrop"
-  >
-  </el-tree>
+  <el-scrollbar>
+    <el-tree
+      ref="tree"
+      v-loading="loading"
+      :data="finalConfig.data"
+      :empty-text="finalConfig.emptyText"
+      :node-key="finalConfig.nodeKey"
+      :props="finalConfig.props"
+      :render-after-expand="finalConfig.renderAfterExpand"
+      :load="finalConfig.load"
+      :render-content="finalConfig.renderContent"
+      :highlight-current="finalConfig.highlightCurrent"
+      :default-expand-all="finalConfig.defaultExpandAll"
+      :expand-on-click-node="finalConfig.expandOnClickNode"
+      :check-on-click-node="finalConfig.checkOnClickNode"
+      :auto-expand-parent="finalConfig.autoExpandParent"
+      :default-expanded-keys="finalConfig.defaultCheckedKeys"
+      :show-checkbox="finalConfig.showCheckbox"
+      :check-strictly="finalConfig.checkStrictly"
+      :default-checked-keys="finalConfig.defaultCheckedKeys"
+      :current-node-key="finalConfig.currentNodeKey"
+      :filter-node-method="finalConfig.filterNodeMethod"
+      :accordion="finalConfig.accordion"
+      :indent="finalConfig.indent"
+      :icon="finalConfig.icon"
+      :lazy="finalConfig.lazy"
+      :draggable="finalConfig.draggable"
+      :allow-drag="finalConfig.allowDrag"
+      :allow-drop="finalConfig.allowDrop"
+      @node-click="handleNodeClick"
+      @node-contextmenu="handleNodeContextmenu"
+      @check-change="handleCheckChange"
+      @check="handleCheck"
+      @current-change="handleCurrentChange"
+      @node-expand="handleNodeExpand"
+      @node-collapse="handleNodeCollapse"
+      @node-drag-start="handleNodeDragStart"
+      @node-drag-enter="handleNodeDragEnter"
+      @node-drag-leave="handleNodeDragLeave"
+      @node-drag-over="handleNodeDragOver"
+      @node-drag-end="handleNodeDragEnd"
+      @node-drop="handleNodeDrop"
+    >
+    </el-tree>
+  </el-scrollbar>
 </template>
 <script setup>
 import { ref, defineEmits, reactive, computed, handleError } from "vue";
@@ -51,30 +53,29 @@ let tree = ref();
 
 let loading = reactive(false);
 let initConfig = reactive({
-    data:[],
-    emptyText: "",
-    nodeKey: "",
-    props: Object,
-    renderAfterExpand: true,
-    load: "",
-    renderContent: null,
-    highlightCurrent: false,
-    defaultExpandAll: false,
-    expandOnClickNode: true,
-    autoExpandParent: true,
-    showCheckbox: false,
-    checkStrictly: false,
-    defaultCheckedKeys: [],
-    currentNodeKey: null,
-    filterNodeMethod: null,
-    accordion: false,
-    indent: 18,
-    icon: null,
-    lazy: false,
-    draggable: false,
-    allowDrag: null,
-    allowDrop: null
-
+  data: [],
+  emptyText: "",
+  nodeKey: "",
+  props: Object,
+  renderAfterExpand: true,
+  load: "",
+  renderContent: null,
+  highlightCurrent: false,
+  defaultExpandAll: false,
+  expandOnClickNode: true,
+  autoExpandParent: true,
+  showCheckbox: false,
+  checkStrictly: false,
+  defaultCheckedKeys: [],
+  currentNodeKey: null,
+  filterNodeMethod: null,
+  accordion: false,
+  indent: 18,
+  icon: null,
+  lazy: false,
+  draggable: false,
+  allowDrag: null,
+  allowDrop: null,
 });
 
 const props = defineProps({
@@ -98,33 +99,62 @@ let finalConfig = computed({
   },
 });
 
-const handleNodeClick = (nodeObecjt, nodeProperty, treeNode, event)=>{
-    emit("node-click", nodeObecjt, nodeProperty, treeNode, event)
-}
-const handleNodeContextmenu = (event, nodeObject, node, self)=>{
-    emit("node-contextmenu", event, nodeObject, node, self)
-}
-const handleCheckChange = (data, selected, selectedObject)=>{
-    emit("check-change",data, selected, selectedObject)
-}
-const handleCheck = (dataObject, node)=>{
-    emit("check", dataObject, node)
-}
-const handleCurrentChange = (data, node)=>{
-    emit("current-change", data, node)
-}
-const handleNodeExpand = ()=>{}
-const handleNodeCollapse = ()=>{}
-const handleNodeDragStart = ()=>{}
-const handleNodeDragEnter = ()=>{}
-const handleNodeDragLeave = ()=>{}
-const handleNodeDragOver = ()=>{}
-const handleNodeDragEnd = ()=>{}
-const handleNodeDrop = ()=>{}
+const handleNodeClick = (nodeObecjt, nodeProperty, treeNode, event) => {
+  emit("node-click", nodeObecjt, nodeProperty, treeNode, event);
+};
+const handleNodeContextmenu = (event, nodeObject, node, self) => {
+  emit("node-contextmenu", event, nodeObject, node, self);
+};
+const handleCheckChange = (data, selected, selectedObject) => {
+  emit("check-change", data, selected, selectedObject);
+};
+const handleCheck = (dataObject, node) => {
+  emit("check", dataObject, node);
+};
+const handleCurrentChange = (data, node) => {
+  emit("current-change", data, node);
+};
+const handleNodeExpand = (data, node, nodeObject) => {
+  emit("node-expand", data, node, nodeObject);
+};
+const handleNodeCollapse = (data, node, nodeObject) => {
+  emit("node-collapse", data, node, nodeObject);
+};
+const handleNodeDragStart = (node, event) => {
+  emit("node-drag-start", node, event);
+};
+const handleNodeDragEnter = (dragedNode, draggingNode, event) => {
+  emit("node-drag-enter", dragedNode, draggingNode, event);
+};
+const handleNodeDragLeave = (dragedNode, draggingNode, event) => {
+  emit("node-drag-leave", dragedNode, draggingNode, event);
+};
+const handleNodeDragOver = (dragedNode, draggingNode, event) => {
+  emit("node-drag-over", dragedNode, draggingNode, event);
+};
+const handleNodeDragEnd = (dragedNode, draggingNode, position, event) => {
+  emit("node-drag-end", dragedNode, draggingNode, position, event);
+};
+const handleNodeDrop = (dragedNode, draggingNode, position, event) => {
+  emit("node-drop", dragedNode, draggingNode, position, event);
+};
 
-function updateTree(){}
+function updateTree() {}
 
-function
-
-const emit = defineEmits(["update:config"]);
+const emit = defineEmits([
+  "update:config",
+  "node-click",
+  "node-contextmenu",
+  "check-change",
+  "check",
+  "current-change",
+  "node-expand",
+  "node-collapse",
+  "node-drag-start",
+  "node-drag-enter",
+  "node-drag-leave",
+  "node-drag-over",
+  "node-drag-end",
+  "node-drop",
+]);
 </script>
