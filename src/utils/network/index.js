a1212138f7eb39479bde178aaed7eb006621fc67
yerikshu <treestore@foxmail.com>
2022-11-13 16:18:47 +0800

    网络配置

---
+++ src/utils/network/index.js
@@ -0,0 +1,5 @@
+import { settings as networkSettings } from "./network";
+
+export const settings = {
+  NETWORK_SETTINGS: networkSettings,
+};
---
+++ src/utils/network/network.js
@@ -0,0 +1,13 @@
+/**
+ * @descrption 请求通用配置
+ */
+export const settings = {
+    BASE_URL: 'http://127.0.0.1:5000',
+    // 最长请求时间
+    REQUEST_TIMEOUT: 60000,
+    // 配置后端数据的接收方式
+    CONTENT_TYPE: "application/json;charset=UTF-8",
+    // 正常处理状态码
+    SUCCESS_CODE: 200,
+    
+}
